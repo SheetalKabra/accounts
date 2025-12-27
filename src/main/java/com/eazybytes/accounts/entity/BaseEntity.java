81e16f8205a0593fd9849ee7527c9b31775e4c3d
Sheetal Kabra <sheetalkabra4@gmail.com>
2025-12-27 16:00:16 +0530

    created API to create the account and customer, create global exceptions to hadle

--- src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -2,9 +2,11 @@ package com.eazybytes.accounts.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -13,12 +15,22 @@ import java.time.LocalDateTime;
 @ToString
 @MappedSuperclass
 public class BaseEntity {
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
     @Column(updatable = false)
     private String createdBy;
-    @Column(insertable = false)
+//    @Column(insertable = false)
     private LocalDateTime updatedAt;
-    @Column(insertable = false)
+//    @Column(insertable = false)
     private String updatedBy;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.createdBy = "123";
+        this.updatedAt = LocalDateTime.now();
+        this.updatedBy = "123";
+    }
+
 }
---
+++ src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,50 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.constants.AccountsConstants;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()){
+            throw new CustomerAlreadyExistException("Customer already registered with the given mobile number " +customerDto.getMobileNumber());
+        }
+        Customer  savedCustomer = customerRepository.save(customer);
+        Accounts savedAccounts = accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    /**
+     *
+     * @param customer - Customer Object
+     * @return the new accounts details
+     */
+
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randAccNumber = 1000000000L + new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+}
